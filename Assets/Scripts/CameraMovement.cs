d6380ead12fd8cab67577a7d8a254569d2018c82
Modhi-Altammami <modi.tammami@gmail.com>
2023-04-06 12:02:13 +0300

    Update CameraMovement.cs

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -23,30 +23,30 @@ namespace MODI.MissionPlan
             //left / right
             if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < 530)
             {
-                targetPos.x=targetPos.x+2;
+                targetPos.x=targetPos.x+5;
             }
             if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > 80)
             {
-                targetPos.x = targetPos.x - 2;
+                targetPos.x = targetPos.x - 5;
             }
             // front / back
             if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.z < 300)
             {
-                targetPos.z = targetPos.z + 2;
+                targetPos.z = targetPos.z + 5;
             }
             if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.z > 40)
             {
-                targetPos.z = targetPos.z - 2;
+                targetPos.z = targetPos.z - 5;
             }
             // up / down
             if (Input.GetKeyDown(KeyCode.Space) && transform.position.y > 100)
             {
-                targetPos.y = targetPos.y - 2;
+                targetPos.y = targetPos.y - 5;
             }
 
             if (Input.GetKeyDown(KeyCode.Escape) && transform.position.y < 300)
             {
-                targetPos.y = targetPos.y + 2;
+                targetPos.y = targetPos.y + 5;
             }
 
             transform.position = targetPos;
